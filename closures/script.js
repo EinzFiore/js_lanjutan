fb4eb333cc386d1f84d74bab54501a30c03e5f45
EinzFiore <ahmadjunaedi1412@gmail.com>
2020-07-09 20:50:19 +0700

    closures | inner function

--- closures/script.js
+++ closures/script.js
@@ -14,3 +14,41 @@
 // function hello(){
 //     return `halo nama saya ${this.nama}, umur saya ${this.umur} tahun.`;
 // }
+
+// Inner Function
+
+// function init(){
+//     let nama = 'Enjun';
+//     function panggilNama(){
+//         console.log(nama);
+//     }
+//     panggilNama();
+// }
+// init();
+
+// function init(){
+//     return function (nama){
+//         console.log(nama);
+//     }
+// }
+
+// let panggilNama = init();
+// panggilNama('Enjun');
+// panggilNama('Junaedi');
+
+
+let add = (function() {
+    let counter = 0;
+    return function() {
+        return ++counter;
+    }
+})();
+
+let counter = 100;
+
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
