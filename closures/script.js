2c8eb2d4c87a90f5e5b4c7e77bf3d4f1d67e2bb9
EinzFiore <ahmadjunaedi1412@gmail.com>
2020-07-09 20:14:25 +0700

    closures 1

---
+++ closures/script.js
@@ -0,0 +1,16 @@
+// Hoisting
+// Menaikan suatu 
+
+
+
+
+// console.log(hello()); Jika posisi console.log berada diatas dari deklarasi variable, maka nilai akan berisi "undefined";
+
+// var nama = 'Enjun';
+// var umur = 20;
+
+// console.log(hello());
+
+// function hello(){
+//     return `halo nama saya ${this.nama}, umur saya ${this.umur} tahun.`;
+// }
